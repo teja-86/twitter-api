b21f52a6567e35c6fc276d919f27c701e990c805
Ranga Dharma Teja Kuntumalla <rangadharmateja0@gmail.com>
2024-10-13 15:34:57 +0530

    Completed

---
+++ backend/server.js
@@ -0,0 +1,53 @@
+import path from 'path';
+import express from "express";
+
+import authRoutes from './routes/auth.route.js';
+import userRoutes from './routes/user.route.js';
+
+import dotenv from 'dotenv';
+import connectMongoDB from './db/connectMongoDB.js';
+import cookieParser from "cookie-parser";
+import {v2 as cloudinary} from 'cloudinary'
+import postRoutes from './routes/post.route.js'
+import notificationRoutes from './routes/notification.route.js'
+
+dotenv.config();
+
+cloudinary.config({
+    cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+    api_key : process.env.CLOUDINARY_API_KEY,
+    api_secret : process.env.CLOUDINARY_API_SECRET
+
+})
+
+const app = express();
+const PORT = process.env.PORT ||5000;
+const __dirname = path.resolve();
+
+app.use(express.json({limit : "5mb"})) // to parse the req.body
+app.use(express.urlencoded({extended : true})) // to parse the form data
+app.use(cookieParser()) // for cookie parser 
+app.use((req, res, next) => {
+    res.setHeader('Cache-Control', 'no-store');
+    next();
+  });
+  
+
+app.use('/api/auth', authRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/posts', postRoutes);
+app.use('/api/notifications', notificationRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    })
+}
+
+app.listen(PORT, () => {
+    console.log(`server is running on the port number ${PORT}`);
+    connectMongoDB();
+})
+
+
